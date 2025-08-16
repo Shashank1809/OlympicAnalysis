8692092faee329768b23e894d3d7444ec9048a3e
shash <shashankjagtap1809@gmail.com>
2025-08-16 17:25:11 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import preprocessor, helper
+from helper import medal_tally
+
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+df = preprocessor.preprocess(df, region_df)
+
+st.sidebar.title("Olympics Analysis")
+user_menu = st.sidebar.radio(
+    'Select option',
+    ('Medal Tally','Overall Analysis','Country-wise Analytics','Athlete-wise Analysis')
+)
+
+if user_menu == 'Medal Tally':
+    st.sidebar.header('Medal Tally')
+    years, country = helper.country_year_list(df)
+
+    selected_year = st.sidebar.selectbox("Select Year", years)
+    selected_country = st.sidebar.selectbox("Select Country", country)
+
+    medal_tally = helper.medal_tally(df)
+    st.dataframe(medal_tally)
