b049febac6a123dee0447e11c5ffa0e2d651ba93
shash <shashankjagtap1809@gmail.com>
2025-09-01 00:21:19 +0530

    Overall Analysis radio button updation

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import preprocessor, helper
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 from helper import medal_tally
 
 df = pd.read_csv('athlete_events.csv')
@@ -21,5 +24,66 @@ if user_menu == 'Medal Tally':
     selected_year = st.sidebar.selectbox("Select Year", years)
     selected_country = st.sidebar.selectbox("Select Country", country)
 
-    medal_tally = helper.medal_tally(df)
-    st.dataframe(medal_tally)
+    medal_tally = helper.fetch_medal_tally(df, selected_year, selected_country)
+    if selected_year == 'Overall' and selected_country == 'Overall':
+        st.title('Overall Tally')
+    if selected_year != 'Overall' and selected_country == 'Overall':
+        st.title("Medal Tally in " + str(selected_year) + " Olympics")
+    if selected_year == 'Overall' and selected_country != 'Overall':
+        st.title(selected_country+ " overall performance")
+    if selected_country != 'Overall' and selected_country != 'Overall':
+        st.title(selected_country + " performance in " + str(selected_year) + " Olympics")
+
+    st.table(medal_tally)
+
+if user_menu == 'Overall Analysis':
+    editions = df['Year'].unique().shape[0] - 1
+    cities = df['City'].unique().shape[0]
+    sports =  df['Sport'].unique().shape[0]
+    events =  df['Event'].unique().shape[0]
+    athletes =  df['Name'].unique().shape[0]
+    nations =  df['region'].unique().shape[0]
+
+    st.title("Top Statistics")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header('Editions')
+        st.title(editions)
+    with col2:
+        st.header('Hosts')
+        st.title(cities)
+    with col3:
+        st.header('Sport')
+        st.title(sports)
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header('Events')
+        st.title(events)
+    with col2:
+        st.header('Nations')
+        st.title(nations)
+    with col3:
+        st.header('Athletes')
+        st.title(athletes)
+
+    nations_over_time = helper.data_over_time(df,'region')
+    fig = px.line(nations_over_time, x="Year", y="region")
+    st.title("Participating Nations Over the Years")
+    st.plotly_chart(fig)
+
+    events_over_time = helper.data_over_time(df, 'Event')
+    fig = px.line(events_over_time, x="Year", y="Event")
+    st.title("Events Over the Years")
+    st.plotly_chart(fig)
+
+    athletes_over_time = helper.data_over_time(df, 'Name')
+    fig = px.line(athletes_over_time, x="Year", y="Name")
+    st.title("Athletes Over the Years")
+    st.plotly_chart(fig)
+
+    st.title("No. of Events over time(Every Sport)")
+    fig,ax = plt.subplots(figsize = (20,20))
+    x = df.drop_duplicates(['Year','Sport','Event'])
+    ax = sns.heatmap(x.pivot_table(index='Sport',columns='Year', values='Event', aggfunc='count').fillna(0).astype(int),annot=True)
+    st.pyplot(fig)
--- helper.py
+++ helper.py
@@ -1,5 +1,28 @@
 import numpy as np
 
+def fetch_medal_tally(df, year, country):
+    medal_df = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
+
+    flag = 0
+    if year == 'Overall' and country == 'Overall':
+        temp_df = medal_df
+    if year == 'Overall' and country != 'Overall':
+        flag = 1
+        temp_df = medal_df[medal_df['region'] == country]
+    if year != 'Overall' and country == 'Overall':
+        temp_df = medal_df[medal_df['Year'] == int(year)]
+    if year != 'Overall' and country != 'Overall':
+        temp_df = medal_df[(medal_df['Year'] == year) & (medal_df['region'] == country)]
+
+    if flag == 1:
+        x = temp_df.groupby('Year').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Year').reset_index()
+    else:
+        x = temp_df.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending=False).reset_index()
+
+    x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
+
+    return x
+
 def medal_tally(df):
     medal_tally =  df.drop_duplicates(subset=['Team','NOC','Games','Year','City','Sport','Event','Medal'])
     medal_tally = medal_tally.groupby('region').sum()[['Gold','Silver','Bronze']].sort_values(by='Gold', ascending=False).reset_index()
@@ -18,3 +41,8 @@ def country_year_list(df):
     country.insert(0, 'Overall')
 
     return years, country
+
+def data_over_time(df,col):
+    nations_over_time = df.drop_duplicates(['Year',col])['Year'].value_counts().reset_index().sort_values('Year')
+    nations_over_time.rename(columns={'Year':'Year','count':col}, inplace=True)
+    return nations_over_time
